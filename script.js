e8e5e5d1ca1f66d928906b8a4809b430bc458287
joevert <joao_marcos99@hotmail.com>
2024-04-27 17:38:17 -0300

    Create files, basic structure.
    add css style, flexbox, implement function
    to create squares

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function criarGrade() {
+    const container = document.querySelector('.container');
+    for (let i = 0; i < 256; i++) {
+        const square = document.createElement("div");
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+    
+}
+
+criarGrade();
